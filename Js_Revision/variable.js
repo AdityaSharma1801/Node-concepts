612facc666bb9d8d212b178d00df2b02a07aa092
aditya sharma <sharmaaditya18012004@gmail.com>
2024-07-29 11:16:37 +0530

    concept of var let const

---
+++ Js_Revision/variable.js
@@ -0,0 +1,43 @@
+// #Scope of the variable
+// #reassignment
+// #redeclaration
+// #mutablity  (changes allowed)
+// #immutablity (changes not allowed)
+// #closure
+// #hosting
+
+// {
+//     var a = 10;
+// }
+
+// function variable(){
+//     {
+//         var a = 5;
+//         console.log(a);
+//     }
+//     console.log(a);
+// }
+
+// variable()
+// console.log(a)
+
+
+
+                            //Hosting
+
+// console.log(a);
+// var a = 5;
+// console.log(a);
+
+// a = 5;            //by default var because of mutablity(state update;chnages allowed), immutablity,reassignment(overlap),redeclaration
+// a =10;            
+// console.log(a);
+
+
+// a = 5;
+// function variable(){
+//     console.log(a);
+// }
+// variable()
+// console.log(a);
+
